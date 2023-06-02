01d1dc74e0c5d13d14d100ad5d1f1e8db6bbf1c8
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-06-02 15:32:36 +0200

    refactored tasks

--- tasks.py
+++ tasks.py
@@ -1,67 +1,84 @@
-import os
+from pathlib import Path
+from typing import Callable
 
+from logger import get_logger
+from schema import ForecastSchema
+from utils import RANDOM_FOLDER_NAME, RANDOM_FILE_NAME
 from yandex_weather_api import YandexWeatherAPI
 
-
-def task_test_0():
-    return get_weather_for_city(CITY_NAME="MOSCOW")
+logger = get_logger()
 
 
-def task_test_1():
-    return get_weather_for_city(CITY_NAME="PARIS")
+def create_folder() -> None:
+    for i in range(5):
+        path = Path(f'{RANDOM_FOLDER_NAME} {i + 1}')
+        if not path.is_dir():
+            path.mkdir()
+            logger.info('Папка создана - %s.', path)
+    logger.info('Папка создана')
 
 
-def task_test_2():
-    return get_weather_for_city(CITY_NAME="LONDON")
+def delete_folder() -> None:
+    for i in range(5):
+        path = Path(f'{RANDOM_FOLDER_NAME} {i + 1}')
+        if path.is_dir():
+            path.rmdir()
+            logger.info('Удаляем папку ... %s.', path)
+    logger.info('Папка удалена!')
 
 
-def task_test_3():
-    return get_weather_for_city(CITY_NAME="BERLIN")
+def create_file() -> None:
+    with open(RANDOM_FILE_NAME, 'w', encoding='UTF-8') as file:
+        message = 'Файл {} успешно создан.'.format(RANDOM_FILE_NAME)
+        file.write(f'{message}\n')
+    logger.info(msg=message)
 
 
-def task_test_4():
-    return get_weather_for_city(CITY_NAME="BEIJING")
+def write_down_to_file() -> None:
+    with open(RANDOM_FILE_NAME, 'a', encoding='UTF-8') as file:
+        file.writelines(['Lorem {i + 1}\n' for i in range(10)])
+    logger.info('Запись в файл окончена.')
 
 
-def task_test_inner_0():
-    return get_weather_for_city(CITY_NAME="BEIJING")
+def read_from_file() -> None:
+    try:
+        with open(RANDOM_FILE_NAME, 'r', encoding='UTF-8') as file:
+            for line in file.readlines():
+                logger.info(line.strip())
+        logger.info('Чтение файла {} окончено.'.format(RANDOM_FILE_NAME))
+    except EnvironmentError as err:
+        logger.error(err)
 
 
-def task_test_inner_1():
-    return get_weather_for_city(CITY_NAME="KAZAN")
+def delete_file() -> None:
+    path = Path(RANDOM_FILE_NAME)
+    if path.is_file():
+        path.unlink()
+        logger.info('Удаляем файл ... %s.', path)
+    logger.info('Файл удалён!')
 
 
-def task_test_inner_2():
-    return get_weather_for_city(CITY_NAME="SPETERSBURG")
+def get_forecasts() -> None:
+    try:
+        forecasts = ForecastSchema.parse_obj(YandexWeatherAPI().get_forecasting())
+        logger.info('Данные извлечены.')
+    except Exception as err:
+        logger.error(err)
+    logger.info('Прогноз погоды извлечён.')
 
 
-def task_test_inner_3():
-    return get_weather_for_city(CITY_NAME="VOLGOGRAD")
+def run_task(task: str) -> Callable:
+    return tasks[task]
 
 
-def get_weather_for_city(CITY_NAME):
-    if not os.path.exists(CITY_NAME):
-        os.makedirs(CITY_NAME)
-    else:
-        os.rmdir(CITY_NAME)
+tasks = {
+    'create_folder': create_folder,
+    'delete_folder': delete_folder,
 
-    yandexAPI = YandexWeatherAPI()
+    'create_file': create_file,
+    'write_to_file': write_down_to_file,
+    'read_from_file': read_from_file,
+    'delete_file': delete_file,
 
-    response = yandexAPI.get_forecasting(CITY_NAME)
-    with open(f"{CITY_NAME}.txt", "w") as file:
-        file.write(str(response.get("info")))
-    return response.get("info")
-
-
-test_tasks = {
-    'task_test_0': task_test_0,
-    'task_test_1': task_test_1,
-    'task_test_2': task_test_2,
-    'task_test_3': task_test_3,
-    'task_test_4': task_test_4,
-
-    'task_test_inner_0': task_test_inner_0,
-    'task_test_inner_1': task_test_inner_1,
-    'task_test_inner_2': task_test_inner_2,
-    'task_test_inner_3': task_test_inner_3,
+    'get_forecasts': get_forecasts
 }
